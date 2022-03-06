2bc3b4fed0bbd4d075ab64e42a54c5cca1d9d678
Rodanna <100126548+Rodanna@users.noreply.github.com>
2022-03-06 21:11:19 +0100

    Update Lightrays.py

--- Lightrays.py
+++ Lightrays.py
@@ -18,8 +18,8 @@ def trfun(x,y):
     #return x+(2*x+y)*(x**2+y**2+x*y)**2/norm, 2*y+(2*y+x)*(x**2/10+y**2/5+x*y)**2/norm #drop
     #return x*np.sqrt(np.abs(1-y**2/2))/100, y*np.sqrt(np.abs(1-x**2/2))/100
     #return x*y/50,y*1.2
-    return 1/(np.sqrt(1-q**2))*np.arctan(np.sqrt(1-q**2)*x/(R+s)), 1/(np.sqrt(1-q**2))*np.arctan(np.sqrt(1-q**2)*y/(R+q**2*s))
-   # return 2*x/(1+x**2+q*y**2)*C, 2*q*y/(1+x**2+q*y**2)*C
+    #return 1/(np.sqrt(1-q**2))*np.arctan(np.sqrt(1-q**2)*x/(R+s)), 1/(np.sqrt(1-q**2))*np.arctan(np.sqrt(1-q**2)*y/(R+q**2*s))
+    return 2*x/(1+x**2+q*y**2)*C, 2*q*y/(1+x**2+q*y**2)*C
 
 
 def display(fname):
