a6e773ce30d38ff080421cfeffba0a79c98ba99f
Prasenjit Saha <psaha@physik.uzh.ch>
2022-03-07 10:54:38 +0100

    spline.py

--- spline.py
+++ spline.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import RectBivariateSpline
 
-q = 0.8
+q = 0.5
 C= 6000
 R = 0.01
 s = 500
@@ -11,6 +11,10 @@ sn = np.sin(np.pi/4)
 
 f = plt.imread('monsters.png')
 f = f[:,:,0]
+t = 0*f
+t[80:100,380:400] = f[80:100,380:400]
+t[145:155,245:255] = 1
+f = t
 plt.imshow(f)
 plt.show()
 
@@ -21,9 +25,11 @@ X,Y = np.meshgrid(u,v)
 
 
 spline = RectBivariateSpline(X[0,:],Y[:,0],f.T)
-x1,y1 = X-2*X/(1+X**2+q*Y**2)*C, Y-2*q*Y/(1+X**2+q*Y**2)*C
+eps = 1
+x1,y1 = X-2*X/(eps+X**2+q*Y**2)*C, Y-2*q*Y/(eps+X**2+q*Y**2)*C
 x2,y2 = X+100*np.exp(X*1j)/(1+X**2+q*Y**2)*C, Y+100*np.exp(Y*1j)/(1+X**2+q*Y**2)*C
 g = spline.ev(x1,y1)
+print(np.max(f),np.max(g))
 h = spline.ev(x2,y2)
 
 
