58cd9032e662475466c679abda6f1bb19833e6d2
Rodanna <100126548+Rodanna@users.noreply.github.com>
2022-03-15 20:51:35 +0100

    Update Hubble.py

--- Hubble.py
+++ Hubble.py
@@ -16,16 +16,16 @@ s = 50
 a0 = 1
 aL = 1.5
 steps = 1000
-
+nr = 8
 
 f = plt.imread('HUBBLE.jpg')
 f = f[:,:,0]
+ny,nx = f.shape
 plt.imshow(f)
 plt.show()
 
 
-ff = np.array([f*0,f*0,f*0,f*0,f*0,f*0,f*0,f*0])
-nr = len(ff)
+ff = np.empty((8,nx,ny),float)
 ff[0][980:1050,970:1050] = f[980:1050,970:1050]
 ff[1][980:1070,210:300] = f[980:1070,210:300]
 ff[2][470:510,500:550] = f[470:510,500:550]
@@ -42,11 +42,9 @@ ff[7][490:570,710:780] = f[490:570,710:780]
 ff[7][730:800,400:450] = f[730:800,400:450]
 
 z = np.array([9,1.8,2,5,2.1,7,6,3,8])
-a = z*0
-Ds = z*0
-Dds = z*0
-A = np.empty((nr,steps), float)
-B = np.empty((nr,steps), float)
+a,Ds,Dds = z*0,z*0,z*0
+A = np.empty((nr,steps),float)
+B = np.empty((nr,steps),float)
 
 for i in range (0,nr):
     a[i] = Distances.scalefactor(z[i])
@@ -56,20 +54,19 @@ for i in range (0,nr):
     Dds[i] = Distances.angular(B[i])[0]
 
 
-ny,nx = f.shape
 u = np.linspace(-nx/2,nx/2,nx)
 v = np.linspace(-ny/2,ny/2,ny)
 X,Y = np.meshgrid(u,v)
 x,y = np.meshgrid(u,v)
-g = np.empty((nr,nx,ny),float)
+g,x,y = np.empty((nr,nx,ny),float)
 x = np.empty((nr,nx,ny),float)
 y = np.empty((nr,nx,ny),float)
-spline = np.array([RectBivariateSpline(X[0,:],Y[:,0],ff[0].T),RectBivariateSpline(X[0,:],Y[:,0],ff[1].T),RectBivariateSpline(X[0,:],Y[:,0],ff[2].T),RectBivariateSpline(X[0,:],Y[:,0],ff[3].T),RectBivariateSpline(X[0,:],Y[:,0],ff[4].T),RectBivariateSpline(X[0,:],Y[:,0],ff[5].T),RectBivariateSpline(X[0,:],Y[:,0],ff[6].T),RectBivariateSpline(X[0,:],Y[:,0],ff[7].T)])
 
 
 for i in range (0,nr):
     x[i],y[i] = X-(Ds[i]/Dds[i])*2*X/(1+X**2+q*Y**2)*C, Y-(Ds[i]/Dds[i])*2*q*Y/(1+X**2+q*Y**2)*C
-    g[i] = spline[i].ev(x[i],y[i])
+    spline = RectBivariateSpline(X[0,:],Y[:,0],ff[i].T)
+    g[i] = spline.ev(x[i],y[i])
     plt.imshow(ff[i])
     plt.show()
     plt.imshow(g[i])
