0309a2032bbee0aa2cc3039b87cfe6523496ef22
Rodanna <100126548+Rodanna@users.noreply.github.com>
2022-03-21 22:11:01 +0100

    Update Deflection angle.py

--- Deflection angle.py
+++ Deflection angle.py
@@ -12,7 +12,7 @@ steps = 1000
 G = 6.67e-11 # m**3/(kg s**2)
 c = 300000000 # m/s
 M = 5e14*2e30 # kg
-beta = 30
+beta = 25
 
 zS = 1.41 #redshift of Q0957+561
 aS = Distances.scalefactor(zS)
@@ -29,4 +29,4 @@ DL = Distances.angular(np.linspace(aS,aL,steps))[0] #cs
 def alpha(x):
     return 4*G*M/c**2 *Dls/(Ds*DL)*x/np.abs(x)**2
 
-print('An estimate for a realistic deflection angle for a Beta =', beta, 'degrees, is Theta =', alpha(beta),'degrees')
+print('An estimate for the deflection angle: Beta =', beta, 'degrees -> Theta =', alpha(beta),'degrees')
