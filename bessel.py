71ea4c07a9f8b89b736fb67f739baaae7dbfc88e
Rodanna <100126548+Rodanna@users.noreply.github.com>
2022-03-24 14:31:03 +0100

    Update bessel.py

--- bessel.py
+++ bessel.py
@@ -23,24 +23,55 @@ def bisection(x,v,tol):
     elif np.sign(special.jv(v,b)) == np.sign(special.jv(v,m)):
         return bisection(x[0:y],v,tol) # m closer than b
 
-v = np.array([0,1,2,3,4])
-x = np.linspace(4,6,1000)
+v = np.arange(0,6)
+x = np.linspace(0,65,1000)
+nrroots = 100
 
 plt.figure()
 plt.grid(True)
-for i in range (0,len(v)):
+for i in range (2,3):
     plt.plot(x,special.jv(v[i],x)) #bessel functions of first order
 plt.show()
 
-'''plt.figure()
+'''plt.figure() #first derivative
 plt.grid(True)
 for i in range (0,len(v)):
-    plt.plot(x,special.jvp(v[i],x,n=1)) #first derivative
+    plt.plot(x,special.jvp(v[i],x,n=1)) 
 plt.show()'''
 
-for i in range (0,len(v)):
-    r1 = bisection(x,v[i],0.01)
-    if r1 != 999:
-        print("FOR J",i,'THE ROOT IS', r1)
+m = np.zeros(len(v))
+xmin = np.empty((len(v),nrroots),float)
+xmax = np.empty((len(v),nrroots),float)
 
-    #print("f(r1) =", f(r1))
+for n in range(0,nrroots):
+    xmin[0][n] = np.pi*(n+0.75)
+    xmax[0][n] = np.pi*(n+0.8)
+    if xmin[0][n] >= x[0] and xmax[0][n] <= x[-1]:
+        m[0] += 1
+    elif xmin[0][n] <= x[0] and xmax[0][n] >= x[0]:
+        r1 = bisection(np.linspace(x[0],xmax[0][n],1000),v[0],0.001)
+        if r1 != 999:
+            m[0] += 1
+    elif xmin[0][n] <= x[-1] and xmax[0][n] >= x[-1]:
+        r1 = bisection(np.linspace(xmin[0][n],x[-1],1000),v[0],0.001)
+        if r1 != 999:
+            m[0] += 1
+print(m[0])
+
+
+for k in range(1,len(v)):
+    for n in range(0,nrroots):
+        xmin[k][n] = xmin[k-1][n]+ 1  # should it be pi?
+        xmax[k][n] = xmax[k-1][n]+ np.pi/2
+        if xmin[k][n] >= x[0] and xmax[k][n] <= x[-1]:
+            m[k] += 1
+        elif xmin[k][n] <= x[0] and xmax[k][n] >= x[0]:
+            r1 = bisection(np.linspace(x[0],xmax[k][n],1000),v[k],0.01)
+            if r1 != 999:
+                m[k] += 1
+        elif xmin[k][n] <= x[-1] and xmax[k][n] >= x[-1]:
+            r1 = bisection(np.linspace(xmin[k][n],x[-1],1000),v[k],0.01)
+            if r1 != 999:
+                m[k] += 1
+    print(m[k])
+    
