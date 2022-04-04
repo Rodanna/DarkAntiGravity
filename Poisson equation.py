607d8f29cce71045707369bc25a83829f6d99bfe
Rodanna <100126548+Rodanna@users.noreply.github.com>
2022-04-04 14:52:38 +0200

    Update Poisson equation.py

--- Poisson equation.py
+++ Poisson equation.py
@@ -9,20 +9,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import special
 
-m = np.arange(0,10)
-a = np.ones(len(m),float)
-b = np.ones(len(m),float)
-alpha = np.ones(len(m),float)
-rmax = 80
+nr, root = np.loadtxt('roots.txt', unpack=True)
+a = np.ones(len(nr),float)
+b = np.ones(len(nr),float)
+
+rmax = 1
 mass = 100
 
-v = np.arange(0,20)
-x = np.linspace(-100,100,250)
-y = np.linspace(-100,100,250)
-X,Y = np.meshgrid(x,y)
-Z = np.zeros((len(x),len(y)),float)
-rho = np.zeros((len(x),len(y)),float) # point mass distribution
+u = np.linspace(-1,1,256)
+X,Y = np.meshgrid(u,u)
+r = np.sqrt(X**2+Y**2)
+phi = np.arctan2(Y,X)
 
+   
+for i in range(0,len(nr)):
+    m = nr[i]
+    km = root[i]
+    z = special.jv(m,km*r/rmax) * (a[i]*np.cos(m*phi)+b[i]*np.sin(m*phi))
+    plt.clf()
+    plt.gca().set_aspect('equal')
+    plt.contourf(X,Y,z,cmap='RdYlBu')
+    plt.pause(0.5)
+
+
+'''
+rho = np.zeros((len(x),len(y)),float) # point mass distribution
 rho[180][130] = mass
 rho[130][120] = mass 
 rho[150][100] = mass
@@ -35,44 +46,24 @@ rho[89][160] = mass
 
 plt.figure()
 plt.imshow(rho)
-plt.show()
+plt.show()'''
 
-def fourierbessel(x,y,m):
+'''def laplace(x,y):
     psi = 0
-    for M in m:
-        psi += a[M]*np.sin(M/np.tan(y/x)) + b[M]*np.cos(M/np.tan(y/x))*special.jv(v[M],np.sqrt(x**2+y**2)*alpha[M]/rmax)
+    r = np.sqrt(x**2+y**2)
+    phi = np.arctan2(y,x)
+    for M in range(0,len(number)):
+        psi += (a[M]*np.sin(M*phi)+ b[M]*np.cos(M*phi))*(root[M]/rmax)**2*(special.jvp(number[M],r*root[M]/rmax,n=2)+(rmax/r*alpha[M]))*special.jvp(number[M],r*root[M]/rmax,n=1)
+        psi += a[M]*M*np.cos(M*phi) - b[M]*M*np.sin(M*phi)*special.jv(number[M],r*root[M]/rmax)/r
     return psi
 
-def laplace(x,y,m):
-    psi = 0
-    for M in m:
-        psi += (a[M]*np.sin(M/np.tan(y/x))+ b[M]*np.cos(M/np.tan(y/x)))*(alpha[M]/rmax)**2*(special.jvp(v[M],np.sqrt(x**2+y**2)*alpha[M]/rmax,n=2)+(rmax/(np.sqrt(x**2+y**2)*alpha[M]))*special.jvp(v[M],np.sqrt(x**2+y**2)*alpha[M]/rmax,n=1))
-        psi += a[M]*M*np.cos(M/np.tan(y/x)) - b[M]*M*np.sin(M/np.tan(y/x))*special.jv(v[M],np.sqrt(x**2+y**2)*alpha[M]/rmax)/np.sqrt(x**2+y**2)
-    return psi
-
-def gradient(x,y,m):
+def gradient(x,y):
     xgrad = 0
     ygrad = 0
-    for M in m:
-        xgrad += (a[M]*np.sin(M/np.tan(y/x)) + b[M]*np.cos(M/np.tan(y/x)))*(np.sqrt(x**2+y**2)*alpha[M]/rmax)*special.jvp(v[M],np.sqrt(x**2+y**2)*alpha[M]/rmax,n=1)
-        ygrad += a[M]*M*np.cos(M/np.tan(y/x)) - b[M]*M*np.sin(M/np.tan(y/x))*(np.sqrt(x**2+y**2)*alpha[M]/rmax)*special.jv(v[M],np.sqrt(x**2+y**2)*alpha[M]/rmax)/np.sqrt(x**2+y**2)
-    return xgrad,ygrad
+    r = np.sqrt(x**2+y**2)
+    phi = np.arctan2(y,x)
+    for M in range(0,len(number)):
+        xgrad += (a[M]*np.sin(M*phi) + b[M]*np.cos(M*phi))*(r*root[M]/rmax)*special.jvp(number[M],r*root[M]/rmax,n=1)
+        ygrad += a[M]*M*np.cos(M*phi) - b[M]*M*np.sin(M*phi)*(r*root[M]/rmax)*special.jv(number[M],r*root[M]/rmax)/r
+    return xgrad,ygrad'''
 
-
-for i in range(0,len(x)):
-    for j in range(0,len(y)):
-        if np.sqrt(X[0,i]**2+Y[j,0]**2) <= rmax:
-            Z[i,j] = fourierbessel(X[0,i],Y[j,0],m)
-            
-plt.figure()
-plt.imshow(Z)
-plt.show()
-
-for i in range(0,len(x)):
-    for j in range(0,len(y)):
-        if np.sqrt(X[0,i]**2+Y[j,0]**2) <= rmax:
-            Z[i,j] = laplace(X[0,i],Y[j,0],m)
-
-plt.figure()
-plt.imshow(Z)
-plt.show()
