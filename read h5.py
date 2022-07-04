0c17c4f62c9fb77aedc95bfef8a19badbf1d309b
Rodanna <100126548+Rodanna@users.noreply.github.com>
2022-07-04 14:04:37 +0200

    Update read h5.py

--- read h5.py
+++ read h5.py
@@ -18,17 +18,25 @@ with h5py.File(filename, "r") as f:
     a_group_key = list(f.keys())[0]
     ds_arr = f[a_group_key][()]  # returns as a numpy array
 
+dens = np.zeros_like(ds_arr)
 rmax = 150 #microradians
 res = 4000
 
 u = np.linspace(-rmax,rmax,res)
 X,Y = np.meshgrid(u,u)
-r = np.sqrt(X**2+Y**2) 
-phi = np.arctan2(Y,X)
+
+for i in range(0,len(X)):
+    for j in range(0,len(Y)):
+        mi = int(res/2) - i + 400
+        mj = int(res/2) - j - 1175
+        if np.sqrt(mi**2+mj**2) <= 500:
+            dens[i,j] = np.exp(ds_arr[i,j])
 
 plt.clf()
 plt.title('mass distribution')
 plt.gca().set_aspect('equal')
-plt.contourf(X,Y,ds_arr,cmap='RdYlBu')
+plt.contourf(X[1760:3040,200:1480],Y[1760:3040,200:1480],dens[1760:3040,200:1480],cmap='RdYlBu')
 plt.colorbar()
 plt.show()
+
+np.savetxt('galaxy.txt',dens[1760:3040,200:1480])
