9b8a1b03d024c06c70b13ccf9b9f771c93d95fe6
Rodanna <100126548+Rodanna@users.noreply.github.com>
2022-08-16 17:02:38 +0200

    Add files via upload

--- GalaxyLensing.py
+++ GalaxyLensing.py
@@ -7,6 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import RectBivariateSpline
 import Distances
+import math
 
 a0 = 1
 aL = 0.6
@@ -25,11 +26,18 @@ Ygrad = -np.loadtxt('Ygrad2_256.txt')
 potential = np.loadtxt('potential2_256.txt')
 
 hf = plt.imread('HUBBLE.jpg')
-hf = hf[1038:1154,239:355,0]
+hf = hf[1050:1154,251:355,0]
 f = 0*X
-xshape = int(np.shape(hf)[0]/2) #only works if difference is an even number
-yshape = int(np.shape(hf)[1]/2)
-f[res//2-yshape:res//2+yshape,res//2-xshape:res//2+xshape] = hf
+xshape1 = int(np.shape(hf)[0]/2) #round down
+xshape2 = int(math.ceil(np.shape(hf)[0]/2)) #round up
+yshape1 = int(np.shape(hf)[1]/2)
+yshape2 = int(math.ceil(np.shape(hf)[1]/2))
+x0 = 3
+y0 = -3
+xdisp = res//2 + x0
+ydisp = res//2 + y0
+
+f[ydisp-yshape1:ydisp+yshape2,xdisp-xshape1:xdisp+xshape2] = hf
 plt.imshow(f)
 plt.show()
 
@@ -41,37 +49,26 @@ for i in range(0,len(z)):
     critdens = (c*Ds)/(4*np.pi*G*Dd*Dds) #kg/m^2
     x,y = X-(Dds/Ds)*Xgrad/critdens, Y-(Dds/Ds)*Ygrad/critdens  
     
-    for k in range(0,10):
-        for t in range(0,10):
-            if not (t==8 and k==7):
-                continue
-            #f = f*0
-            #f[55+k,55+t] = 1
-            x0 = -5 + t
-            y0 = -5 + k
-            rad2 = (X-x0)**2 + (Y-y0)**2
-            #f = np.exp(-rad2/5)
-            spline = RectBivariateSpline(u,u,f.T)
-            g = spline.ev(x,y)
-            plt.title('source image')
-            plt.imshow(f.T,origin='lower',extent=[-rmax,rmax,-rmax,rmax])
-            plt.pause(1)
-            plt.clf()
-            plt.title('lensed images')
-            plt.imshow(g.T,origin='lower',extent=[-rmax,rmax,-rmax,rmax])
-            plt.pause(2)
-            plt.clf()
+    spline = RectBivariateSpline(u,u,f.T)
+    g = spline.ev(x,y)
+    plt.title('source image')
+    plt.imshow(f.T,origin='lower',extent=[-rmax,rmax,-rmax,rmax])
+    plt.pause(1)
+    plt.clf()
+    plt.title('lensed images')
+    plt.imshow(g.T,origin='lower',extent=[-rmax,rmax,-rmax,rmax])
+    plt.pause(2)
+    plt.clf()
             
-            tnodim = ((X-x0)**2 + (Y-y0)**2)/2 + potential/critdens*(Dds/Ds)
-            tau = (1+zL)*Ds*Dd/Dd*tnodim #10^-9 s due to unit microrad
-            tau /= 1e12  #arrival time surface in s
-            tmin = np.min(tau)
-            tmax = np.max(tau)
-            levs = np.linspace(tmin,tmin + (tmax-tmin)/5,200)
-            cs = plt.contour(Y,X,tau,levels=levs)
-            #plt.colorbar(cs)
-            plt.title('arrival time surface')
-            plt.gca().set_aspect('equal')
-            plt.title('%i %i' % (t,k))
-            plt.pause(2)
-            plt.clf()
+    tnodim = ((X-x0)**2 + (Y-y0)**2)/2 + potential/critdens*(Dds/Ds)
+    tau = (1+zL)*Ds*Dd/Dd*tnodim #10^-12 s due to unit microrad
+    tau /= 1e12  #arrival time surface in s
+    tmin = np.min(tau)
+    tmax = np.max(tau)
+    levs = np.linspace(tmin,tmin + (tmax-tmin)/5,50)
+    cs = plt.contour(Y,X,tau,levels=levs)
+    #plt.colorbar(cs)
+    plt.title('arrival time surface')
+    plt.gca().set_aspect('equal')
+    plt.pause(0.1)
+    plt.clf()
