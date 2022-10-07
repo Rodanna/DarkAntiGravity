dbb4f58b2f583eaf81572025bea910be8f9976cd
Rodanna <100126548+Rodanna@users.noreply.github.com>
2022-10-07 14:14:23 +0200

    Add files via upload

--- GalaxyCutouts.py
+++ GalaxyCutouts.py
@@ -13,30 +13,32 @@ f = plt.imread('HUBBLE.jpg')
 f = f[:,:,0]
 ny,nx = f.shape
 
-a = np.array([630,626,518])
-b = np.array([680,651,530])
-c = np.array([640,497,712])
-d = np.array([690,521,724])
+a = np.array([630,626,518,980,0])
+b = np.array([680,651,530,1050,1024])
+c = np.array([640,497,712,965,0])
+d = np.array([690,521,724,1035,1024])
 
-ff = np.empty((4,nx,ny),float)
+ff = np.empty((5,nx,ny),float)
 ff[0][a[0]:b[0],c[0]:d[0]] = f[a[0]:b[0],c[0]:d[0]]
 ff[1][a[1]:b[1],c[1]:d[1]] = f[a[1]:b[1],c[1]:d[1]]
 ff[2][a[2]:b[2],c[2]:d[2]] = f[a[2]:b[2],c[2]:d[2]]
-ff[3] = f
+ff[3][a[3]:b[3],c[3]:d[3]] = f[a[3]:b[3],c[3]:d[3]] 
+ff[4] = f
 
 for i in range (0,len(ff)):
     plt.title('source image')
     plt.imshow(ff[i].T,origin='lower',extent=[-rmax,rmax,-rmax,rmax])
     plt.pause(1)
+    plt.show()
     plt.clf()
 
 
-for i in range (0,3):
+for i in range (0,len(ff)):
     f = plt.imread('HUBBLE.jpg')[:,:,0]
     f = f[a[i]:b[i],c[i]:d[i]]
     plt.figure(frameon=False)
     plt.axis('off')
     plt.imshow(f.T)
-    plt.savefig(f'cutout{i}.jpg',bbox_inches='tight',pad_inches = 0)
+    plt.savefig(f'cutout{i}.jpg',dpi = 4, bbox_inches='tight',pad_inches = 0)
     plt.pause(1)
     plt.clf()
--- GalaxyLensing.py
+++ GalaxyLensing.py
@@ -10,39 +10,46 @@ import Distances
 import math
 
 a0 = 1
-aL = 0.6
+aL = 0.65
 zL = 1/aL-1
 rmax = 150
-res = 500
+rmax2 = 150
+res = 2048
 G = 6.67408e-11 #m^3/kgs^2
 c = 299792458 #m/s
 z = np.linspace(9,13,8)
 u = np.linspace(-rmax,rmax,res)
 X,Y = np.meshgrid(u,u)
 
-Xgrad = -np.loadtxt('Xgrad2_500.txt')
-Ygrad = -np.loadtxt('Ygrad2_500.txt')
-potential = np.loadtxt('potential2_500.txt')
+Xgrad = -np.loadtxt('Xgrad2_2048.txt')
+Ygrad = -np.loadtxt('Ygrad2_2048.txt')
+potential = np.loadtxt('potential2_2048.txt')
 
-hf = plt.imread('HUBBLE.jpg')
+#hf = plt.imread('HUBBLE.jpg')
+#hf = hf[:,:,0]
 #hf = hf[1050:1154,251:355,0]
-hf = hf[626:651,497:521,0]
-#hf = plt.imread('cutout2.jpg')[:,:,0]
+#hf = hf[626:651,497:521,0]
+hf = plt.imread('cutout2.jpg')[:,:,1]
 f = 0*X
 yshape1 = int(np.shape(hf)[0]/2) #round down
 yshape2 = int(math.ceil(np.shape(hf)[0]/2)) #round up
 xshape1 = int(np.shape(hf)[1]/2)
 xshape2 = int(math.ceil(np.shape(hf)[1]/2))
-x0 = 3
-y0 = -3
+x0 = 0
+y0 = 0
 xdisp = res//2 + x0
 ydisp = res//2 + y0
 
 f[ydisp-yshape1:ydisp+yshape2,xdisp-xshape1:xdisp+xshape2] = hf
-plt.imshow(f.T)
-plt.show()
 
-for i in range(0,len(z)):
+plt.title('Hubble Deep Field')
+plt.imshow(hf,origin='lower',extent=[-rmax2,rmax2,-rmax2,rmax2])
+plt.xlabel('x in microradians')
+plt.ylabel('y in microradians')
+plt.show()
+plt.clf()
+
+for i in range(len(z)-1,len(z)):
     asrc = Distances.scalefactor(z[i])
     Ds = Distances.angular(a0,asrc)
     Dds = Distances.angular(aL,asrc)
@@ -56,20 +63,26 @@ for i in range(0,len(z)):
     plt.imshow(f.T,origin='lower',extent=[-rmax,rmax,-rmax,rmax])
     plt.pause(1)
     plt.clf()
-    plt.title('lensed images')
+    plt.title('Lensed Hubble Deep Field')
+    plt.xlabel('x in microradians')
+    plt.ylabel('y in microradians')
     plt.imshow(g.T,origin='lower',extent=[-rmax,rmax,-rmax,rmax])
     plt.pause(2)
     plt.clf()
-            
+    
     tnodim = ((X-x0)**2 + (Y-y0)**2)/2 + potential/critdens*(Dds/Ds)
-    tau = (1+zL)*Ds*Dd/Dd*tnodim #10^-12 s due to unit microrad
-    tau /= 1e12  #arrival time surface in s
+    tau = (1+zL)*Ds*Dd/Dds*tnodim #10^-9 s due to unit microrad
+    tau /= 1e12  #arrival time surface in s                           
     tmin = np.min(tau)
     tmax = np.max(tau)
-    levs = np.linspace(tmin,tmin + (tmax-tmin)/5,50)
-    cs = plt.contour(Y,X,tau,levels=levs)
-    plt.colorbar(cs)
-    plt.title('arrival time surface')
+    levs = np.linspace(tmin,tmin + (tmax-tmin)/5,100)
     plt.gca().set_aspect('equal')
+    plt.contour(Y,X,tau,levels=levs)
+    plt.grid()
+    plt.title('arrival time surface')
+    plt.xlabel('x in microradians')
+    plt.ylabel('y in microradians')
+    #plt.savefig(f'timedelay{i}')
     plt.pause(0.1)
     plt.clf()
+    
